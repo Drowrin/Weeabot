893121b8d5092bc377f16573e0426dddc23294e9
Drowrin <drowrin@gmail.com>
2016-08-04 14:19:05 -0400

    clean zalgo a bit

--- cogs/pointless.py
+++ cogs/pointless.py
@@ -11,24 +11,6 @@ from cleverbot import Cleverbot
 from utils import *
 
 
-def _insert_randoms(text):
-    random_extras = [chr(i) for i in range(0x1D023, 0x1D045 + 1)]
-    newtext = []
-    for char in text:
-        newtext.append(char)
-        if random.randint(1, 5) == 1:
-            newtext.append(random.choice(random_extras))
-    return u''.join(newtext)
-
-
-def _is_narrow_build():
-    try:
-        chr(0x10000)
-    except ValueError:
-        return True
-    return False
-
-
 class Pointless(SessionCog):
     """Nothing to see here."""
 
@@ -57,21 +39,19 @@ class Pointless(SessionCog):
     @text_command.command(pass_context=True, aliases=('z',))
     async def zalgo(self, ctx):
         """ZALGO"""
-        text = ctx.invoked_with.join(ctx.message.clean_content.split(ctx.invoked_with)[1:])
+        source = ctx.invoked_with.join(ctx.message.clean_content.split(ctx.invoked_with)[1:]).upper()
         zalgo_chars = [chr(i) for i in range(0x0300, 0x036F + 1)]
         zalgo_chars.extend([u'\u0488', u'\u0489'])
-        source = text.upper()
-        if not _is_narrow_build:
-            source = _insert_randoms(source)
+        random_extras = [chr(i) for i in range(0x1D023, 0x1D046)]
+        source = ''.join([s + random.choice(random_extras) if random.randint(1, 5) == 1 else None for s in source])
         zalgoized = []
         for letter in source:
             zalgoized.append(letter)
             zalgo_num = random.randint(0, 25) + 1
             for _ in range(zalgo_num):
                 zalgoized.append(random.choice(zalgo_chars))
-        response = random.choice(zalgo_chars).join(zalgoized)
         await self.bot.delete_message(ctx.message)
-        await self.bot.reply(response)
+        await self.bot.reply(''.join(zalgoized))
 
     @text_command.command(pass_context=True, aliases=('e',))
     async def emoji(self, ctx):
