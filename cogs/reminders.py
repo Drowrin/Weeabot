a715610abcd4daafca713f402fec0e116faf708a
Hawkfiend <jordan.bates1996@yahoo.com>
2016-11-11 03:08:55 -0500

    initial reminders system. Will change

---
+++ cogs/reminders.py
@@ -0,0 +1,76 @@
+import asyncio
+import re
+import traceback
+import time
+# noinspection PyUnresolvedReferences
+import discord
+# noinspection PyUnresolvedReferences
+from discord.ext import commands
+from utils import *
+
+
+class Reminders:
+    """Handles sending reminders to users."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        if 'reminders' not in self.bot.status:
+            self.bot.status['reminders'] = {}
+        self.bot.loop.create_task(self.check_reminders())
+
+    @commands.command(pass_context=True)
+    async def remindme(self, ctx, message, *, duration: str):
+        """The bot will send you a reminder. Make sure the message is in quotes if it is not one word.
+
+        If the bot is restarted, the timing will potentially be less accurate by a few minutes."""
+        def gettime(s: str, d: str):
+            try:
+                r = re.search("\d[\d.]*\s*{}".format(s), d)
+                return int(re.match("\d+", r.group()).group())
+            except (TypeError, ValueError, AttributeError):
+                return 0
+        seconds = gettime('s', duration)
+        seconds += gettime('m', duration) * 60
+        seconds += gettime('h', duration) * 3600
+        seconds += gettime('d', duration) * 86400
+        current_time = int(time.time())
+        finished = current_time + seconds
+        await self.bot.say('I will remind you: "{}" in {} seconds. Does this sound correct?'.format(message, seconds))
+        m = await self.bot.wait_for_message(author=ctx.message.author, channel=ctx.message.channel)
+        if 'y' in m.content:
+            await self.bot.say("\N{OK HAND SIGN}")
+        else:
+            await self.bot.say("Cancelled.")
+            return
+        self.bot.status['reminders'][ctx.message.id] = {
+            'finished': finished,
+            'channel': ctx.message.channel.id,
+            'author': ctx.message.author.mention,
+            'message': message
+        }
+        self.bot.dump_status()
+        await asyncio.sleep(seconds)
+        await self.bot.say('{} I was told to remind you: "{}"'.format(ctx.message.author.mention, message))
+        del self.bot.status['reminders'][ctx.message.id]
+        self.bot.dump_status()
+
+    async def check_reminders(self):
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed:
+            dellist = []
+            for mid, r in self.bot.status['reminders'].items():
+                if r['finished'] < time.time():
+                    await self.bot.send_message(discord.Object(
+                        id=r['channel']),
+                        '{} I was told to remind you: "{}"\nNote: timing is innacurate. the bot crashed/restarted.'
+                        .format(r['author'], r['message'])
+                    )
+                    dellist.append(mid)
+            for m in dellist:
+                del self.bot.status['reminders'][m]
+                self.bot.dump_status()
+            await asyncio.sleep(180)
+
+
+def setup(bot):
+    bot.add_cog(Reminders(bot))
