f4ec6c65c78a0d815d0f850dfe9f926fb9f7c669
Drowrin <drowrin@gmail.com>
2017-01-10 20:30:56 -0500

    fix ~do

--- cogs/pointless.py
+++ cogs/pointless.py
@@ -142,7 +142,7 @@ class Pointless(utils.SessionCog):
     @commands.command(pass_context=True)
     async def do(self, ctx, n: int, *, command):
         """Repeat a command up to 5 times."""
-        banned = [f'{self.bot.commad_prefix}req', '{self.bot.commad_prefix}do']
+        banned = [f'{self.bot.command_prefix}req', f'{self.bot.command_prefix}do']
         if any([x in command for x in banned]):
             await self.bot.say(f"That command may not be used in {self.bot.commad_prefix}do.")
             return
