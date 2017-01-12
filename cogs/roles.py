9cb1aa9d31b6a8b8d91ac456d91800aaddc2a6fc
Adam <admsvy@gmail.com>
2017-01-12 02:09:27 -0500

    Added opt-in roles cog.

---
+++ cogs/roles.py
@@ -0,0 +1,84 @@
+from collections import defaultdict
+
+import discord
+from discord.ext import commands
+
+from cogs.requestsystem import request, RequestLevel
+from cogs.spoilers import SpoilerChannel
+from Weeabot import Weeabot
+
+class Roles:
+	
+	 def __init__(self, bot: Weeabot):
+		self.bot = bot
+
+	async def check_config( self, ctx ):
+		if ctx.message.server.id not in self.bot.server_configs:
+            self.bot.server_configs[ctx.message.server.id] = {}
+        if 'hidden_channels' not in self.bot.server_configs[ctx.message.server.id]:
+            self.bot.server_configs[ctx.message.server.id]['hidden_channels'] = {}
+			
+	async def get_roles_list( self, ctx ):
+		await self.update_roles()
+		roles_list = defaultdict(list)
+		await self.check_config( ctx )
+		for chan, roles in bot.server_configs[ ctx.message.server ][ "hidden_channels" ].items():
+			chann = ctx.message.server.get_channel( chan )
+			for role in roles: 
+				roles_list[ role ].append( chann )
+		return roles_list
+		
+	async def update_roles( self, ctx ):		
+		for chan_id, r in bot.server_configs[ ctx.message.server ][ 'hidden_channels' ].items():
+			roles = [ t[0].id for t in ctx.message.server.get_channel(id).overwrites if t[1].read_messages ]		
+			bot.server_configs[ ctx.message.server ][ 'hidden_channels' ][chan_id] = roles
+	
+	@commands.command(pass_context=True)
+	@checks.is_server_owner()
+	async def hide( self, ctx ):
+		await self.check_config( ctx )
+		await self.bot.edit_channel_permissions(
+            channel=ctx.message.channel,
+            target=ctx.message.server.default_role,
+            overwrite=discord.PermissionOverwrite(read_messages=False)
+        )
+		await self.update_roles( ctx )
+		
+	@commands.command(pass_context=True)
+	@request(level=RequestLevel.server, owner_bypass=False)
+	async def make_channel( self, ctx, channel_name, role_name ):	
+		await self.check_config( ctx )
+		 try:
+            everyone_perms = discord.PermissionOverwrite(read_messages=False)
+            everyone = discord.ChannelPermissions(target=ctx.message.server.default_role, overwrite=everyone_perms)
+            can_read = discord.PermissionOverwrite(read_messages=True)
+			new_role = await self.bot.create_role( ctx.message.server, name=role_name )
+            channel = await self.bot.create_channel(ctx.message.server, name, everyone, ( new_role , can_read ))	
+			await bot.add_roles( ctx.message.author, new_role )
+			bot.server_configs[ ctx.message.server ][ 'hidden_channels' ][ channel.id ] = [ new_role.id ]
+			
+        except discord.errors.HTTPException:
+            await self.bot.say("Invalid name or that name is taken. Names must be alphanumeric.")
+	
+	
+	@commands.command(pass_context=True)
+	async def roles( self, ctx ):
+		roles = await self.get_roles_list()
+		message = ""
+		for role, servs in roles.items():
+			message = message + r.name for r in ctx.message.server.roles if r.id == role.id + "\n"
+			for server in servs:
+				message = message + "\t" + server.name + "\n\t\t" + server.topic + "\n"
+		await bot.say( message )
+	
+	@commands.command(pass_context=True)
+	async def makeme(self, ctx, role : discord.Role):
+		roles = await self.get_roles_list( ctx )
+		if role.id not in roles:
+			await bot.say( "Sorry, that role isn't an opt-in role.")
+			return		 
+		await bot.add_roles( ctx.message.author, role )
+
+def setup(bot):
+    bot.add_cog(Roles(bot))
+	
