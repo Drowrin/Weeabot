719beb8e9bebe8f871bd79f8c22fe54ca5c2d65c
Drowrin <drowrin@gmail.com>
2017-06-12 17:58:42 -0400

    testing attachment embeds. will use to clean up tags

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
 
 from weeabot import Weeabot
+from weeabot.core.context import Context
+
+import discord
 
 bot = Weeabot(os.path.join('config', 'config.yml'), command_prefix='$')
 
@@ -14,6 +17,15 @@ async def test(ctx):
 
     bot.reactionlisteners.add(ctx.message, t)
     print(await ctx.confirm('yes/no'))
+
+
+@bot.command()
+async def image(ctx: Context):
+    f = discord.File(os.path.join('images', 'test.png'))
+    await ctx.send(
+        file=f,
+        embed=discord.Embed().set_image(url=f'attachment://{f.filename}')
+    )
 
 
 if __name__ == '__main__':
