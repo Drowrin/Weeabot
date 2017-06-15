7f5b36d3fad31a9e4b987e0da8d688e84cca17b8
Drowrin <drowrin@gmail.com>
2017-06-15 19:23:41 -0400

    add exec to temporary location for testing

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import os
+import re
+import sys
+import traceback
 
 from weeabot import Weeabot
 
@@ -25,6 +28,62 @@ async def image(ctx):
         file=f,
         embed=discord.Embed().set_image(url=f'attachment://{f.filename}')
     )
+
+
+@bot.command(pass_context=True, name='exec')
+async def execute(ctx, *, code: str):
+    """Code is executed as a coroutine and the return is output to this channel."""
+    lang = re.match(r'```(\w+\s*\n)', code)
+    code = code.strip('` ')
+    if lang:
+        code = code[len(lang.group(1)):]
+    block = f'```{lang[1] if lang else "py"}\n{{}}\n```'
+
+    env = {
+        'bot': bot,
+        'ctx': ctx,
+        'message': ctx.message,
+        'guild': ctx.message.guild,
+        'channel': ctx.message.channel,
+        'author': ctx.message.author
+    }
+    env.update(globals())
+
+    embed = discord.Embed(
+        description=block.format(code)
+    ).set_footer(
+        text=f'Python {sys.version.split()[0]}',
+        icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/200px-Python-logo-notext.svg.png'
+    )
+
+    # prepare to execute
+    lines = code.split('\n')
+    exec('async def _():\n    ' + '\n    '.join(lines) + '\nctx.exec = _', env)
+
+    embed.color = discord.Colour.blue()
+    embed.title = "\N{TIMER CLOCK}"
+    msg = await ctx.send(embed=embed)
+
+    # clean up code afterwards
+    embed.description = None
+
+    try:
+        result = await ctx.exec()
+        if result is not None:
+            embed.add_field(
+                name='Result',
+                value=block.format(result)
+            )
+        embed.colour = discord.Colour.green()
+        embed.title = "\N{OK HAND SIGN}"
+    except Exception as e:
+        embed.add_field(
+            name='Exception',
+            value=block.format(''.join(traceback.format_exception(type(e), e, None)))
+        )
+        embed.colour = discord.Colour.red()
+        embed.title = "\N{CROSS MARK}"
+    await msg.edit(embed=embed)
 
 
 if __name__ == '__main__':
