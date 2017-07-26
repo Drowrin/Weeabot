0e259ef267621d4e13de718cddf1b5bda465b889
Drowrin <drowrin@gmail.com>
2017-07-26 18:33:44 -0400

    update reaction listeners

--- weeabot/cogs/reaction_listeners.py
+++ weeabot/cogs/reaction_listeners.py
@@ -1,7 +1,24 @@
-import asyncio
-
-from weeabot import utils
 from . import base_cog
+
+
+class ReactionListener:
+    def __init__(self, message, callback, single_use=True, user=None):
+        self.message = message
+        self.callback = callback
+        self.single_use = single_use
+        self.user = user
+
+    async def __call__(self, reaction, user):
+        r  = await self.callback(reaction, user)
+        self.single_use = bool(r) if r is not None else self.single_use
+
+    def check(self, reaction, user):
+        if self.user is not None:
+            if user != self.user:
+                return False
+        if reaction.message == self.message:
+            return True
+        return False
 
 
 class ReactionListeners(base_cog(shortcut=True)):
@@ -13,19 +30,28 @@ class ReactionListeners(base_cog(shortcut=True)):
         super(ReactionListeners, self).__init__(bot)
         self.listeners = {}
 
-    def add(self, msg, callback):
+    def add(self, message, callback):
         """
         add a listener to perform an action when a reaction is done on a given message.
 
         Callback should be a coroutine, with the same args as on_reaction_add(reaction, user).
+        Return, if any, is used as bool to determine if this listener should stay.
         Does not persist through restarts.
         """
-        self.listeners[utils.full_id(msg)] = callback
+
+        self.listeners[message.id] = ReactionListener(message, callback)
 
     async def on_reaction_add(self, reaction, user):
         m = reaction.message
-        callback = self.listeners.pop(utils.full_id(m), asyncio.coroutine(lambda *args: None))
-        await callback(reaction, user)
+        callback = self.listeners.get(m.id)
+        if callback is not None and callback.check(reaction, user):
+            await callback(reaction, user)
+            if callback.delete_after:
+                try:
+                    del self.listeners[m.id]
+                except KeyError:
+                    # can happen when multiple reactions happen quickly
+                    pass
 
 
 def setup(bot):
