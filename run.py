69e9cbd3ffcfc2566254955607d2f9cbbb67c3df
Drowrin <drowrin@gmail.com>
2017-08-08 21:58:25 -0400

    remove extra commands
    these are no longer needed for testing

--- run.py
+++ run.py
@@ -11,26 +11,5 @@ import discord
 
 bot = Weeabot(os.path.join('config', 'config.yml'), command_prefix='$')
 
-
-@bot.command()
-async def test(ctx):
-    await ctx.affirmative()
-
-    async def t(reaction, user):
-        print(reaction, user)
-
-    bot.reactionlisteners.add(ctx.message, t)
-    print(await ctx.confirm('yes/no'))
-
-
-@bot.command()
-async def image(ctx):
-    f = discord.File(os.path.join('images', 'test.png'))
-    await ctx.send(
-        file=f,
-        embed=discord.Embed().set_image(url=f'attachment://{f.filename}')
-    )
-
-
 if __name__ == '__main__':
     bot.run()
