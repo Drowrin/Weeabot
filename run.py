d4606bbf08520ec8781e5b5ccf5830ae46d842c3
Drowrin <drowrin@gmail.com>
2018-06-20 00:19:06 -0400

    clean run file

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 import os
-import re
-import sys
-import traceback
-import textwrap
 
 from weeabot import Weeabot
-from weeabot.cogs.stats import do_not_track
-
-import discord
 
 bot = Weeabot(os.path.join('config', 'config.yml'), command_prefix='$')
 
