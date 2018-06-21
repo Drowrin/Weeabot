d20a975d6c77c859d1c65ac66ecdf91be57e928b
Drowrin <drowrin@gmail.com>
2018-06-20 22:01:22 -0400

    use script parameter for config path

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import os
+import sys
 
 from weeabot import Weeabot
 
-bot = Weeabot(os.path.join('config', 'config.yml'))
+
+if len(sys.argv) > 1:
+    config_path = sys.argv[1]
+else:
+    config_path = os.path.join('config', 'config.yml')
+
+bot = Weeabot(config_path)
 
 if __name__ == '__main__':
     bot.run()
