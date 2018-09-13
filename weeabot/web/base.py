e7ce4aaf4d847ab7a58015c47a578757da78acfc
Drowrin <drowrin@gmail.com>
2018-09-12 20:59:19 -0400

    component fix

--- weeabot/web/base.py
+++ weeabot/web/base.py
@@ -1,6 +1,7 @@
 from os import path
 from vibora.blueprints import Blueprint
 from vibora import Response, Request
+from weeabot.core.bot import Weeabot
 from . import templates
 
 
@@ -8,9 +9,9 @@ base = Blueprint('base')
 
 
 @base.route('/')
-async def root(request: Request):
+async def root(request: Request, bot: Weeabot):
     return Response(
-        await templates.render('index.html', request),
+        await templates.render('index.html', request, bot),
         headers={"content-type": "text/html; charset=utf-8"}
     )
 
--- weeabot/web/templates.py
+++ weeabot/web/templates.py
@@ -5,8 +5,7 @@ from weeabot.core.bot import Weeabot
 
 environment = Environment(loader=PackageLoader('weeabot', 'web'), enable_async=True)
 
-async def render(template: str, request: Request, **kwargs):
-    bot = request.get_component(Weeabot)
+async def render(template: str, bot: Weeabot, request: Request, **kwargs):
     return await environment.get_template(template).render_async({
         "request": request,
         "navpages": bot.config['web']['site']['navpages'],
